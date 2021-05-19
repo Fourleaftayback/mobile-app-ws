279a6fc188294d8eb988a855676c0a6444dc5069
jaylivecare <jay@livecareusa.com>
2021-05-18 21:25:25 -0400

    changed the mapping on post requeset

---
+++ src/main/java/com/appdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.appdeveloperblog.app.ws.mobileappws.ui.controller;
+
+import com.appdeveloperblog.app.ws.mobileappws.shared.dto.UserDto;
+import com.appdeveloperblog.app.ws.mobileappws.ui.model.request.UserDetailRequestModel;
+import com.appdeveloperblog.app.ws.mobileappws.ui.response.UserRest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public UserRest createUser(@RequestBody UserDetailRequestModel userDetails) {
+
+        UserRest returnValue = new UserRest(); // creates the userRest object
+        UserDto userDto = new UserDto(); // creates the userDto object
+
+        BeanUtils.copyProperties(userDetails, userDto); // this copies the userDetails object to userDto
+        UserDto createUser = userService.createUser(userDto); // need to create this one this will create the user in the DB
+        BeanUtils.copyProperties(createUser, returnValue); // then will copy the values to the return value so we can send to the client
+        return returnValue;
+
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
+}
+
+
---
+++ src/main/java/com/appdeveloperblog/app/ws/mobileappws/ui/response/UserRest.java
@@ -0,0 +1,40 @@
+package com.appdeveloperblog.app.ws.mobileappws.ui.response;
+
+public class UserRest {
+    private String userId; // this will not be the id from the database but used to send back to the client and to make sure this class is only sending back needed info
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
